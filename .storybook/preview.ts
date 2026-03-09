2cfdb2136ab9089f96733454b3cdd09914317a4f
Eugene Debrah <debraheug@gmail.com>
2026-03-09 21:42:15 +0000

    Initial commit from Create Next App

---
+++ .storybook/preview.ts
@@ -0,0 +1,24 @@
+import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: "neutral-50",
+      values: [
+        { name: "neutral-50", value: "hsl(0 0% 98%)" },
+        { name: "neutral-white", value: "#ffffff" },
+        { name: "neutral-900", value: "hsl(180 4% 11%)" },
+      ],
+    },
+    layout: "centered",
+  },
+};
+
+export default preview;
