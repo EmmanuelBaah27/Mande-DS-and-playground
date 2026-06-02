5749cbd5ca0d3f6bf1e19ddc86198f768d745ceb
emmanuelbaah27 <emmanuelbaah27@gmail.com>
2026-06-02 11:52:57 +0000

    feat(ui): add neutral-a20 ring to Avatar container

--- packages/ui/src/components/ui/avatar.tsx
+++ packages/ui/src/components/ui/avatar.tsx
@@ -32,7 +32,7 @@ const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(
       <div
         ref={ref}
         className={cn(
-          "relative flex shrink-0 items-center justify-center rounded-full",
+          "relative flex shrink-0 items-center justify-center rounded-full border border-neutral-a20",
           sizeClasses[size],
           className
         )}
